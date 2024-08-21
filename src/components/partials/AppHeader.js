19dab9af8f4f65f2f8226c7d62bd6cfe17a6837c
JustinPong <Justinpong0416@gmail.com>
2024-08-21 22:17:33 +0800

    finish basic template of header

--- src/components/partials/AppHeader.js
+++ src/components/partials/AppHeader.js
@@ -2,9 +2,24 @@ import React from "react";
 
 const AppHeader = () => {
   return (
-    <header>
-      header~
-      {/* Your header content goes here */}
+    <header className="bg-[#060606]">
+      <nav className="h-[80px] flex justify-between items-center">
+        <div className="container mx-auto px-5 flex justify-between items-center">
+          <a
+            href="/"
+            className="flex gap-1 w-auto transform transition-transform duration-300 hover:scale-105"
+          >
+            <span className="text-white font-sans text-2xl font-semibold">
+              Justin
+            </span>
+            <span className="text-primary-green font-sans text-2xl font-semibold">
+              Pong
+            </span>
+          </a>
+
+          <div>Menus</div>
+        </div>
+      </nav>
     </header>
   );
 };
