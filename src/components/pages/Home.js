947a788be6504edffb4435ae50a6d70fe6176ac2
JustinPong <Justinpong0416@gmail.com>
2024-08-25 21:15:17 +0800

    finish homepage responsive

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -21,27 +21,27 @@ const Home = () => {
 
   return (
     <section
-      className="min-h-[calc(100vh-80px)] flex"
+      className="mt-6 lg:mt-0 lg:min-h-[calc(100vh-80px)] flex"
       data-component="home-page"
       id="home"
     >
       <div class="container m-auto px-5">
-        <div className="flex items-center gap-36">
-          <div className="w-3/5">
-            <h1 className="text-8xl font-bold mt-6 leading-none font-sans">
+        <div className="flex flex-col-reverse lg:flex-row items-center gap-10 xl:gap-36">
+          <div className="w-full lg:w-3/5">
+            <h1 className="text-5xl lg:text-7xl font-bold mt-4 lg:mt-6 leading-none font-sans">
               Hi, It's <span class="text-primary-green">Justin</span>
             </h1>
-            <h3 className="text-animation text-5xl font-bold mt-4 mb-14 font-sans">
+            <h3 className="text-animation text-2xl lg:text-4xl font-bold mt-4 mb-6 lg:mb-10 font-sans">
               I'm a <span className="text-primary-green" ref={el} />
             </h3>
-            <p className="text-2xl">
+            <p className="text-base lg:text-2xl">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do,
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim, ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut
             </p>
           </div>
-          <div className="w-2/5">
+          <div className="w-full max-w-[600px] lg:w-2/5">
             <img src="/images/home-logo.png" alt="home-logo" />
           </div>
         </div>
