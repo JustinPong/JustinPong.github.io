2b1e5f00bc2851577de4fe0a9c8b203d3adfd32b
JustinPong <Justinpong0416@gmail.com>
2024-08-28 00:25:17 +0800

    setup the cleaner structure for future development in home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,18 +4,47 @@ import Typed from "typed.js";
 const Home = () => {
   const el = React.useRef(null);
 
-  React.useEffect(() => {
-    const typed = new Typed(el.current, {
+  const initTyped = () => {
+    const options = {
       strings: ["Web Developer", "Software Engineer", "Freelancer"],
       typeSpeed: 75,
       loop: true,
       backSpeed: 75,
       backDelay: 100,
-    });
+    };
+
+    const typed = new Typed(el.current, options);
 
     return () => {
-      // Destroy Typed instance during cleanup to stop animation
       typed.destroy();
+    };
+  };
+
+  const initSwiper = () => {
+    // Initialize Swiper here
+    // Return a cleanup function if necessary
+  };
+
+  const initScrollTrigger = () => {
+    // Initialize ScrollTrigger here
+    // Return a cleanup function if necessary
+  };
+
+  const initTimeline = () => {
+    // Initialize Timeline here
+    // Return a cleanup function if necessary
+  };
+
+  React.useEffect(() => {
+    const cleanups = [
+      initTyped(),
+      initSwiper(),
+      initScrollTrigger(),
+      initTimeline(),
+    ];
+
+    return () => {
+      cleanups.forEach((cleanup) => cleanup && cleanup());
     };
   }, []);
 
@@ -25,11 +54,11 @@ const Home = () => {
       data-component="home-page"
       id="home"
     >
-      <div class="container m-auto px-5">
+      <div className="container m-auto px-5">
         <div className="flex flex-col-reverse lg:flex-row items-center gap-10 xl:gap-36">
           <div className="w-full lg:w-3/5">
             <h1 className="text-5xl lg:text-7xl font-bold mt-4 lg:mt-6 leading-none font-sans">
-              Hi, It's <span class="text-primary-green">Justin</span>
+              Hi, It's <span className="text-primary-green">Justin</span>
             </h1>
             <h3 className="text-animation text-2xl lg:text-4xl font-bold mt-4 mb-6 lg:mb-10 font-sans">
               I'm a <span className="text-primary-green" ref={el} />
@@ -42,7 +71,7 @@ const Home = () => {
             </p>
           </div>
           <div className="w-full max-w-[600px] lg:w-2/5">
-            <img src="/images/home-logo.png" alt="home-logo" />
+            <img src="/images/home-logo.png" alt="Home Logo" />
           </div>
         </div>
       </div>
