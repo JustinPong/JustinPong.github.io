a7b834e9d5899e8ffd4fbf67cf1577ffeb5ad4d8
JustinPong <Justinpong0416@gmail.com>
2024-09-02 00:11:22 +0800

    update home landing style

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Typed from "typed.js";
+import PrimaryButton from "../button/PrimaryButton";
 
 const Home = () => {
   const el = React.useRef(null);
 
   const initTyped = () => {
     const options = {
-      strings: ["Web Developer", "Software Engineer", "Freelancer"],
+      strings: ["Web Developer", "JavaScript Enthusiast", "Freelancer"],
       typeSpeed: 75,
       loop: true,
       backSpeed: 75,
@@ -54,21 +55,23 @@ const Home = () => {
       data-component="home-page"
       id="home"
     >
-      <div className="container m-auto px-5">
+      <div className="container m-auto px-20">
         <div className="flex flex-col-reverse lg:flex-row items-center gap-10 xl:gap-36">
           <div className="w-full lg:w-3/5">
-            <h1 className="text-5xl lg:text-7xl font-bold mt-4 lg:mt-6 leading-none font-sans">
-              Hi, It's <span className="text-primary-green">Justin</span>
+            <div class="text-primary font-mono">Hi, my name is</div>
+            <h1 className="text-5xl lg:text-7xl font-bold mt-4 lg:mt-6 leading-none font-sans text-[#e0def4]">
+              Justin Pong.
             </h1>
             <h3 className="text-animation text-2xl lg:text-4xl font-bold mt-4 mb-6 lg:mb-10 font-sans">
-              I'm a <span className="text-primary-green" ref={el} />
+              I'm a <span className="text-primary" ref={el} />
             </h3>
-            <p className="text-base lg:text-2xl">
+            <p className="text-base lg:text-xl">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do,
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim, ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut
             </p>
+            <PrimaryButton text="Get In Touch!" className="mt-[50px]" />
           </div>
           <div className="w-full max-w-[600px] lg:w-2/5">
             <img src="/images/home-logo.png" alt="Home Logo" />
--- src/components/partials/AppHeader.js
+++ src/components/partials/AppHeader.js
@@ -4,7 +4,7 @@ const AppHeader = () => {
   return (
     <header className="bg-[#060606]">
       <div className="h-[80px] flex justify-between items-center">
-        <div className="container mx-auto px-5 flex justify-between items-center">
+        <div className="px-14 w-full flex justify-between items-center">
           <a
             href="/"
             className="flex gap-1 w-auto transform transition-transform duration-300 hover:scale-105"
@@ -12,7 +12,7 @@ const AppHeader = () => {
             <span className="text-white font-sans text-2xl font-semibold">
               Justin
             </span>
-            <span className="text-primary-green font-sans text-2xl font-semibold">
+            <span className="text-primary font-sans text-2xl font-semibold">
               Pong
             </span>
           </a>
