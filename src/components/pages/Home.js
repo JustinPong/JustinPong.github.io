6fecb250483ab20034a789389847d9f4c8205b0f
JustinPong <Justinpong0416@gmail.com>
2024-09-02 23:16:46 +0800

    remove homepage logo

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -51,21 +51,21 @@ const Home = () => {
 
   return (
     <section
-      className="mt-6 lg:mt-0 lg:min-h-[calc(100vh-80px)] flex"
+      className="min-h-[calc(100vh-80px)] flex"
       data-component="home-page"
       id="home"
     >
-      <div className="container m-auto px-20">
+      <div className="container m-auto px-32">
         <div className="flex flex-col-reverse lg:flex-row items-center gap-10 xl:gap-36">
-          <div className="w-full lg:w-3/5">
+          <div className="w-full max-w-[1000px] m-auto">
             <div class="text-primary font-mono">Hi, my name is</div>
             <h1 className="text-5xl lg:text-7xl font-bold mt-4 lg:mt-6 leading-none font-sans text-[#e0def4]">
               Justin Pong.
             </h1>
-            <h3 className="text-animation text-2xl lg:text-4xl font-bold mt-4 mb-6 lg:mb-10 font-sans">
+            <h3 className="text-animation text-2xl lg:text-3xl xl:text-4xl font-bold mt-4 mb-6 lg:mb-10 font-sans">
               I'm a <span className="text-primary" ref={el} />
             </h3>
-            <p className="text-base lg:text-xl">
+            <p className="text-base lg:text-xl max-w-[800px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do,
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim, ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -73,9 +73,9 @@ const Home = () => {
             </p>
             <PrimaryButton text="Get In Touch!" className="mt-[50px]" />
           </div>
-          <div className="w-full max-w-[600px] lg:w-2/5">
+          {/* <div className="w-full max-w-[600px] lg:w-2/5">
             <img src="/images/home-logo.png" alt="Home Logo" />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
