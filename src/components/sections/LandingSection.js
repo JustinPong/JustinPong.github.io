5ccf815dcd5929de4053d63acf38ced66944bef8
JustinPong <Justinpong0416@gmail.com>
2024-09-16 00:04:52 +0800

    remove homepage logo and redesign homepage

--- src/components/sections/LandingSection.js
+++ src/components/sections/LandingSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typed from "typed.js";
 import PrimaryButton from "../button/PrimaryButton";
+import "./LandingSection.css";
 
 const LandingSection = () => {
   const el = React.useRef(null);
@@ -51,15 +52,16 @@ const LandingSection = () => {
 
   return (
     <section
-      className="min-h-[calc(100vh-80px)] flex"
+      className="min-h-[100vh] flex"
       data-component="home-page"
       id="home"
     >
-      <div className="container m-auto px-32">
-        <div className="flex flex-col-reverse lg:flex-row items-center gap-10 2xl:gap-36">
-          <div className="w-full lg:max-w-[400px] xl:max-w-[550px] m-auto">
+      <div className="container m-auto px-5 lg:px-32">
+        <div className="flex flex-col-reverse lg:flex-row gap-10 2xl:gap-36">
+          <div className="w-full lg:max-w-[1000px] m-auto">
             <div class="text-primary font-mono">Hi, my name is</div>
-            <h1 className="text-4xl xl:text-7xl font-bold mt-4 xl:mt-6 leading-none font-sans text-white">
+            {/* text-4xl xl:text-7xl */}
+            <h1 className="big-heading font-bold mt-4 xl:mt-6 leading-none font-sans text-white">
               Justin Pong.
             </h1>
             <h3 className="text-animation text-xl lg:text-2xl xl:text-4xl font-bold mt-4 mb-6 xl:mb-10 font-sans">
@@ -73,9 +75,9 @@ const LandingSection = () => {
             </p>
             <PrimaryButton text="Get In Touch!" className="mt-[50px]" />
           </div>
-          <div className="w-full max-w-[600px] lg:w-2/5">
+          {/* <div className="w-full max-w-[600px] lg:w-2/5">
             <img src="/images/home-logo.png" alt="Home Logo" />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
