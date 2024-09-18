1621edf14106c42f977619e732a24c252e9c5284
JustinPong <Justinpong0416@gmail.com>
2024-09-19 00:32:32 +0800

    add email component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AppHeader from "./components/partials/AppHeader";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/pages/Homepage/Homepage";
+import Email from "./components/fixed-button/email/email";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
+        <Email />
       </Router>
     </div>
   );
---
+++ src/components/fixed-button/email/email.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./email.css";
+
+const Email = () => {
+  return (
+    <div className="email-container">
+      <div className="email-link-wrapper">
+        <a
+          href="mailto:justinpong0416@gmail.com"
+          className="email-link font-mono"
+        >
+          justinpong0416@gmail.com
+        </a>
+        <div className="divider-line"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Email;
